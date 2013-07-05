85213e7e45ba7e86d1b4621856ce6384d4831c8d
Nar Kumar <neokya@gmail.com>
2013-07-06 04:07:25 +0530

    submit on pypi

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ sorl-thumbnail-async
 ====================
 
 Asynchronous thumbnailing app in django with remote storages like S3. This is fork of sorl-thumbnail, which is a bit slow when used with remote storages.
+
+Install
+-------
+
+`pip install sorl-thumbnail-async`
--- setup.py
+++ setup.py
@@ -1,35 +1,18 @@
-import thumbnail
 from setuptools import setup, find_packages
-from setuptools.command.test import test
 
-
-class TestCommand(test):
-    def run(self):
-        from tests.runtests import runtests
-        runtests()
-
+version = '0.4.3'
 
 setup(
     name='sorl-thumbnail-async',
-    version=0.1,
+    version=version,
     description='Asynchronous thumbnailing app in django with remote storages like S3',
     author='Nar Kumar Chhantyal',
     author_email='neokya@gmail.com',
-    license='BSD',
     url='https://github.com/neokya/sorl-thumbnail-async',
-    packages=find_packages(exclude=['tests', 'tests.*']),
-    platforms='any',
+    packages=find_packages(),
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Multimedia :: Graphics',
-        'Framework :: Django',
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.3',
     ],
-    cmdclass={"test": TestCommand},
 )
