d070a8c1d583f208b4f24a9d563bf9e33a473004
Nar Kumar <neokya@gmail.com>
2013-10-16 15:01:26 +0530

    change pypi source url

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
-version = '0.5.1'
+version = '0.5.2'
 
 setup(
     name='sorl-thumbnail-async',
     version=version,
     description='Asynchronous thumbnailing app in django with remote storages like S3',
     author='Nar Kumar Chhantyal',
-    author_email='neokya@gmail.com',
-    url='https://github.com/neokya/sorl-thumbnail-async',
+    author_email='nkchhantyal@gmail.com',
+    url='https://github.com/chhantyal/sorl-thumbnail-async',
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
